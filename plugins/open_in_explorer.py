f3d9ffe72cdb508dc7cd2b75f42f1186f409debd
Marcus Ottosson <konstruktion@gmail.com>
2014-05-28 09:10:39 +0100

    Added plugin

---
+++ plugins/open_in_explorer.py
@@ -0,0 +1,29 @@
+"""Open directory of current file in explorer
+
+Usage:
+    1. Store in $sublime/Packages/User
+    2. Map to hotkey (e.g. F11)
+    3. Press hotkey to open explorer
+
+Tested under Windows 7, 8 and Ubuntu 12.01.
+
+"""
+
+
+import os
+import platform
+import subprocess
+import sublime_plugin
+
+mapping = {'Windows': 'explorer',
+           'Darwin': 'start',
+           'Linux': 'nautilus'}
+
+explorer = mapping.get(platform.system())
+
+
+class OpenInExplorerCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        source_file = self.view.file_name()
+        source_dir = os.path.dirname(source_file)
+        subprocess.Popen([explorer, source_dir])
